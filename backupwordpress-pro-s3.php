da751be16bcec39c1dad26b78738b94ef5c3b04e
paul de wouters <paul@hmn.md>
2014-09-16 17:59:28 -0300

    Refactor main plugin load

--- backupwordpress-pro-s3.php
+++ backupwordpress-pro-s3.php
@@ -27,82 +27,104 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-/**
- * Dependency check. Only activate if BackUpWordPress is active.
- * Ref: http://wcdocs.woothemes.com/codex/extending/create-a-plugin/.
- * http://10up.com/blog/2012/10/wordpress-plug-in-self-deactivation/
- */
-
 defined( 'WPINC' ) or die;
 
-$dependency_check = class_exists( 'HMBKP_Scheduled_Backup' ) || ( function_exists( 'is_plugin_active_for_network' ) && is_plugin_active_for_network( 'backupwordpress/backupwordpress.php' ) );
 
-// Only activate if BackUpWordPress is loaded
-if ( ! $dependency_check ) {
+if ( ! defined( 'HMBKP_S3_REQUIRED_PHP_VERSION' ) ) {
+	define( 'HMBKP_S3_REQUIRED_PHP_VERSION', '5.3.1' );
+}
 
-	add_action( 'admin_init', 'hmbkp_aws_deactivate' );
-	add_action( 'admin_notices', 'hmbkp_aws_admin_notice' );
-	add_action( 'network_admin_notices', 'hmbkp_aws_admin_notice' );
+// Don't activate on anything less than PHP required version
+if ( version_compare( phpversion(), HMBKP_S3_REQUIRED_PHP_VERSION, '<' ) ) {
+	deactivate_plugins( trailingslashit( basename( dirname( __FILE__ ) ) ) . basename( __FILE__ ) );
+	wp_die( sprintf( __( 'BackUpWordPress to Amazon S3 requires PHP version %s or greater.', 'backupwordpress-pro-s3' ), HMBKP_S3_REQUIRED_PHP_VERSION ), __( 'BackUpWordPress to Amazon S3', 'backupwordpress-pro-s3' ), array( 'back_link' => true ) );
+}
 
-	function hmbkp_aws_deactivate() {
+
+function hmbkp_aws_activate() {
+
+	if ( ! defined( 'HMBKP_S3_REQUIRED_WP_VERSION' ) ) {
+		define( 'HMBKP_S3_REQUIRED_WP_VERSION', '3.8.4' );
+	}
+
+
+	if ( ! class_exists( 'HMBKP_Scheduled_Backup' ) ) {
+
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+
+		wp_die( __( 'BackUpWordPress To Amazon S3 requires BackUpWordPress to be activated. It has been deactivated.', 'backupwordpress-pro-gdrive' ), 'BackUpWordPress to Google Drive', array( 'back_link' => true ) );
+
+	}
+
+		// Don't activate on old versions of WordPress
+	global $wp_version;
+
+	if ( version_compare( $wp_version, HMBKP_S3_REQUIRED_WP_VERSION, '<' ) ) {
 		deactivate_plugins( trailingslashit( basename( dirname( __FILE__ ) ) ) . basename( __FILE__ ) );
+		wp_die( sprintf( __( 'BackUpWordPress to Amazon S3 requires WordPress version %s or greater.', 'backupwordpress-pro-s3' ), HMBKP_S3_REQUIRED_WP_VERSION ), __( 'BackUpWordPress to Amazon S3', 'backupwordpress-pro-s3' ), array( 'back_link' => true ) );
+
 	}
 
-	function hmbkp_aws_admin_notice() {
-		echo ( '<div class="error"><p>BackUpWordPress to Amazon s3 requires BackUpWordPress to be installed and activated. Plugin deactivated.</p></div>' );
-		if ( isset( $_GET['activate'] ) )
-			unset( $_GET['activate'] );
-		if ( isset( $_GET['activate-multi'] ) )
-			unset( $_GET['activate-multi'] );
+}
+
+register_activation_hook( __FILE__, 'hmbkp_aws_activate' );
+
+
+function hmbkpp_aws_init() {
+
+
+	if ( ! class_exists( 'HMBKP_Scheduled_Backup' ) ) {
+
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+		wp_die( __( 'BackUpWordPress To Amazon S3 requires BackUpWordPress to be activated. It has been deactivated.', 'backupwordpress-pro-gdrive' ), 'BackUpWordPress to Google Drive', array( 'back_link' => true ) );
+
 	}
 
-} else {
+	if ( ! defined( 'HMBKP_S3_REQUIRED_WP_VERSION' ) ) {
+		define( 'HMBKP_S3_REQUIRED_WP_VERSION', '3.8.4' );
+	}
 
-	if ( ! defined( 'HMBKP_S3_REQUIRED_WP_VERSION' ) )
-		define( 'HMBKP_S3_REQUIRED_WP_VERSION', '3.7.3' );
+	// Don't activate on old versions of WordPress
+	global $wp_version;
 
-	if( ! defined( 'HMBKP_S3_REQUIRED_PHP_VERSION' ) )
-		define( 'HMBKP_S3_REQUIRED_PHP_VERSION', '5.3.1' );
+	if ( version_compare( $wp_version, HMBKP_S3_REQUIRED_WP_VERSION, '<' ) ) {
+		deactivate_plugins( trailingslashit( basename( dirname( __FILE__ ) ) ) . basename( __FILE__ ) );
+		wp_die( sprintf( __( 'BackUpWordPress to Amazon S3 requires WordPress version %s or greater.', 'backupwordpress-pro-s3' ), HMBKP_S3_REQUIRED_WP_VERSION ), __( 'BackUpWordPress to Amazon S3', 'backupwordpress-pro-s3' ), array( 'back_link' => true ) );
 
-	if ( ! defined( 'HMBKP_S3_PLUGIN_SLUG' ) )
+	} else {
+
+		hmbkpp_aws_admin();
+
+		include_once HMBKP_S3_PLUGIN_PATH . 's3/s3.php';
+
+	}
+
+}
+add_action( 'admin_init', 'hmbkpp_aws_init' );
+
+
+/**
+ * Set up plugin, load dependencies, modules and add hooks
+ */
+function hmbkp_aws_plugin_setup() {
+
+	if ( ! defined( 'HMBKP_S3_PLUGIN_SLUG' ) ) {
 		define( 'HMBKP_S3_PLUGIN_SLUG', plugin_basename( dirname( __FILE__ ) ) );
+	}
 
-	if ( ! defined( 'HMBKP_S3_PLUGIN_PATH' ) )
+	if ( ! defined( 'HMBKP_S3_PLUGIN_PATH' ) ) {
 		define( 'HMBKP_S3_PLUGIN_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+	}
 
-	if ( ! defined( 'HMBKP_S3_PLUGIN_URL' ) )
+	if ( ! defined( 'HMBKP_S3_PLUGIN_URL' ) ) {
 		define( 'HMBKP_S3_PLUGIN_URL', trailingslashit( plugin_dir_url( __FILE__ ) ) );
+	}
 
 	// Set filter for plugin's languages directory
-	if( ! defined( 'HMBKP_S3_PLUGIN_LANG_DIR' ) )
+	if ( ! defined( 'HMBKP_S3_PLUGIN_LANG_DIR' ) ) {
 		define( 'HMBKP_S3_PLUGIN_LANG_DIR', apply_filters( 'hmbkp_aws_filter_lang_dir',
 			HMBKP_S3_PLUGIN_PATH . '/languages/' ) );
-
-	function hmbkp_aws_activate() {
-
-		// Don't activate on anything less than PHP required version
-		if ( version_compare( phpversion(), HMBKP_S3_REQUIRED_PHP_VERSION, '<' ) ) {
-			deactivate_plugins( trailingslashit( basename( dirname( __FILE__ ) ) ) . basename( __FILE__ ) );
-			wp_die( sprintf( __( 'BackUpWordPress to Amazon S3 requires PHP version %s or greater.', 'backupwordpress-pro-s3' ), HMBKP_S3_REQUIRED_PHP_VERSION ), __( 'BackUpWordPress to Amazon S3', 'backupwordpress-pro-s3' ), array( 'back_link' => true ) );
-		}
-		// Don't activate on old versions of WordPress
-		global $wp_version;
-
-		if ( version_compare( $wp_version, HMBKP_S3_REQUIRED_WP_VERSION, '<' ) ) {
-			deactivate_plugins( trailingslashit( basename( dirname( __FILE__ ) ) ) . basename( __FILE__ ) );
-			wp_die( sprintf( __( 'BackUpWordPress to Amazon S3 requires WordPress version %s or greater.', 'backupwordpress-pro-s3' ), HMBKP_S3_REQUIRED_WP_VERSION ), __( 'BackUpWordPress to Amazon S3' , 'backupwordpress-pro-s3' ), array( 'back_link' => true ) );
-
-		}
-		// loads the translation files
-		hmbkp_aws_plugin_textdomain();
-
 	}
-
-	register_activation_hook( __FILE__, 'hmbkp_aws_activate' );
-
-	if ( defined( 'HM_DEV') && HM_DEV )
-		@ini_set('date.timezone', 'Europe/Amsterdam');
-
 
 	// this is the URL our updater / license checker pings. This should be the URL of the site with EDD installed
 	define( 'HMBKPP_AWS_STORE_URL', 'https://bwp.hmn.md' ); // you should use your own CONSTANT name, and be sure to replace it throughout this file
@@ -110,52 +132,33 @@ if ( ! $dependency_check ) {
 // the name of your product. This should match the download name in EDD exactly
 	define( 'HMBKPP_AWS_ADDON_NAME', 'BackUpWordPress To Amazon S3' ); // you should use your own CONSTANT name, and be sure to replace it throughout this file
 
-	if ( ! defined( 'HMBKP_S3_PLUGIN_VERSION' ) )
+	if ( ! defined( 'HMBKP_S3_PLUGIN_VERSION' ) ) {
 		define( 'HMBKP_S3_PLUGIN_VERSION', '1.0.7' );
+	}
 
-	if( ! class_exists( 'HMBKPP_SL_Plugin_Updater' ) )
+	if ( ! class_exists( 'HMBKPP_SL_Plugin_Updater' ) ) {
 		include( trailingslashit( dirname( __FILE__ ) ) . 'assets/edd-plugin-updater/HMBKPP-SL-Plugin-Updater.php' );
+	}
 
 	// retrieve our license key from the DB
 	$settings = hmbkpp_aws_fetch_settings();
 
 	$license_key = $settings['license_key'];
 
-// setup the updater
+	// setup the updater
 	$edd_updater = new HMBKPP_SL_Plugin_Updater( HMBKPP_AWS_STORE_URL, __FILE__, array(
-			'version' 	=> HMBKP_S3_PLUGIN_VERSION, 				// current version number
-			'license' 	=> $license_key, 		// license key (used get_option above to retrieve from DB)
-			'item_name' => HMBKPP_AWS_ADDON_NAME, 	// name of this plugin
-			'author' 	=> 'Human Made Limited'  // author of this plugin
+			'version'   => HMBKP_S3_PLUGIN_VERSION,                // current version number
+			'license'   => $license_key,        // license key (used get_option above to retrieve from DB)
+			'item_name' => HMBKPP_AWS_ADDON_NAME,    // name of this plugin
+			'author'    => 'Human Made Limited'  // author of this plugin
 		)
 	);
 
+	// loads the translation files
+	hmbkp_aws_plugin_textdomain();
+}
+add_action( 'plugins_loaded', 'hmbkp_aws_plugin_setup' );
 
-	register_deactivation_hook( __FILE__, 'hmbkpp_aws_deactivate' );
-
-	/**
-	 * Set up plugin, load dependencies, modules and add hooks
-	 */
-	function hmbkp_aws_plugin_setup() {
-
-		hmbkpp_aws_admin();
-
-		// Load S3 if curl is enabled
-		if ( function_exists( 'curl_init' ) )
-			include_once HMBKP_S3_PLUGIN_PATH . 's3/s3.php';
-
-		// Hook into the schedule action menu
-		if( ! has_filter( 'hmbkp_schedule_actions_menu' ) )
-			add_filter( 'hmbkp_schedule_actions_menu', 'hmbkp_aws_append_destination_action', 10, 2 );
-
-		// set up the ajax handler for the destination form
-		add_action( 'wp_ajax_hmbkp_aws_edit_destination_load', 'hmbkp_aws_edit_destination_load' );
-
-	}
-
-	add_action( 'plugins_loaded', 'hmbkp_aws_plugin_setup' );
-
-} // end if
 
 /**
  * Sets default plugin cap level
@@ -165,8 +168,9 @@ function hmbkp_aws_get_manage_cap() {
 
 	static $manage_cap;
 
-	if ( ! empty ( $manage_cap ) )
+	if ( ! empty ( $manage_cap ) ) {
 		return $manage_cap;
+	}
 
 	$manage_cap = apply_filters( 'hmbkp_aws_manage_cap', 'manage_options' );
 
@@ -179,25 +183,25 @@ function hmbkp_aws_get_manage_cap() {
  * This setup allows a user to just drop his custom translation files into the WordPress language directory
  * Files will need to be in a subdirectory with the name of the textdomain 'backupwordpress-pro-s3'
  */
- function hmbkp_aws_plugin_textdomain() {
+function hmbkp_aws_plugin_textdomain() {
 
-	 /** Set unique textdomain string */
-	 $textdomain = 'backupwordpress-pro-s3';
+	/** Set unique textdomain string */
+	$textdomain = 'backupwordpress-pro-s3';
 
-	 /** The 'plugin_locale' filter is also used by default in load_plugin_textdomain() */
-	 $locale = apply_filters( 'plugin_locale', get_locale(), $textdomain );
+	/** The 'plugin_locale' filter is also used by default in load_plugin_textdomain() */
+	$locale = apply_filters( 'plugin_locale', get_locale(), $textdomain );
 
-	 /** Set filter for WordPress languages directory */
-	 $hmbkp_aws_wp_lang_dir = apply_filters(
-		 'hmbkp_aws_filter_wp_lang_dir',
-			 trailingslashit( WP_LANG_DIR ) . trailingslashit( $textdomain )  . $textdomain . '-' . $locale . '.mo'
-	 );
+	/** Set filter for WordPress languages directory */
+	$hmbkp_aws_wp_lang_dir = apply_filters(
+		'hmbkp_aws_filter_wp_lang_dir',
+		trailingslashit( WP_LANG_DIR ) . trailingslashit( $textdomain ) . $textdomain . '-' . $locale . '.mo'
+	);
 
-	 /** Translations: First, look in WordPress' "languages" folder = custom & update-secure! */
-	 load_textdomain( $textdomain, $hmbkp_aws_wp_lang_dir );
+	/** Translations: First, look in WordPress' "languages" folder = custom & update-secure! */
+	load_textdomain( $textdomain, $hmbkp_aws_wp_lang_dir );
 
-	 /** Translations: Secondly, look in plugin's "languages" folder = default */
-	 load_plugin_textdomain( $textdomain, FALSE, HMBKP_S3_PLUGIN_LANG_DIR );
+	/** Translations: Secondly, look in plugin's "languages" folder = default */
+	load_plugin_textdomain( $textdomain, false, HMBKP_S3_PLUGIN_LANG_DIR );
 
 } // end plugin_textdomain
 
@@ -213,10 +217,16 @@ function hmbkp_aws_append_destination_action( $output, $schedule ) {
 
 	return $output .= sprintf(
 		'<a class="colorbox" href="%s">%s</a> | ',
-		add_query_arg( array( 'action' => 'hmbkp_aws_edit_destination_load', 'hmbkp_schedule_id' => $schedule->get_id() ), admin_url( 'admin-ajax.php' ) ),
+		add_query_arg( array( 'action'            => 'hmbkp_aws_edit_destination_load',
+		                      'hmbkp_schedule_id' => $schedule->get_id()
+			), admin_url( 'admin-ajax.php' ) ),
 		__( 'Destinations', 'backupwordpress-pro-s3' )
 	);
 
+}
+
+if ( ! has_filter( 'hmbkp_schedule_actions_menu' ) ) {
+	add_filter( 'hmbkp_schedule_actions_menu', 'hmbkp_aws_append_destination_action', 10, 2 );
 }
 
 /**
@@ -231,26 +241,7 @@ function hmbkp_aws_edit_destination_load() {
 	die();
 
 }
-
-/**
- * Register and load plugin scripts
- */
-function hmbkp_aws_load_scripts() {
-
-	$screen = get_current_screen();
-
-	if ( 'tools_page_backupwordpress' == $screen->id ) {
-
-		wp_enqueue_script(
-			'hmbkp-aws',
-				HMBKP_S3_PLUGIN_URL . 'assets/hmbkp-aws.js',
-			array( 'jquery' ),
-			HMBKP_S3_PLUGIN_VERSION,
-			true
-		);
-
-	} // end if
-}
+add_action( 'wp_ajax_hmbkp_aws_edit_destination_load', 'hmbkp_aws_edit_destination_load' );
 
 /**
  * Include the plugin settings form
@@ -272,6 +263,7 @@ function hmbkpp_aws_deactivate() {
 	delete_option( 'hmbkpp_aws_license_key' );
 
 }
+register_deactivation_hook( __FILE__, 'hmbkpp_aws_deactivate' );
 
 /**
  * Define default settings
@@ -281,7 +273,7 @@ function hmbkpp_aws_deactivate() {
 function hmbkpp_aws_default_settings() {
 
 	$defaults = array(
-		'license_key' => '',
+		'license_key'    => '',
 		'license_status' => ''
 	);
 
