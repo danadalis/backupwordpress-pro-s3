21fee5390176209fa6fd1bc5f9832f14fe5ef150
paul de wouters <paul@hmn.md>
2015-04-12 16:15:15 +0100

    Delete any transient with alternate name

--- uninstall.php
+++ uninstall.php
@@ -10,3 +10,4 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 // delete plugin option
 delete_option( 'hmbkpp_aws_settings' );
 delete_transient( 'hmbkp_license_data_s3' );
+delete_transient( 'hmbkp_aws_license_data' );
