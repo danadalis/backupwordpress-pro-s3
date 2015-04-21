286bde4c93585e0f0c14bfdc46551ef069400394
paul de wouters <paul@hmn.md>
2015-04-21 17:24:55 +0100

    Refactors to account for BWP common changes

--- inc/Config.php
+++ inc/Config.php
@@ -6,6 +6,6 @@ $container['addon_version'] = '2.0.4';
 $container['min_bwp_version'] = '3.1.4';
 $container['edd_download_file_name'] = 'BackUpWordPress To Amazon S3';
 $container['addon_settings'] = 'hmbkpp_aws_settings';
-$container['transient_name'] = 'hmbkp_license_data_s3';
 $container['service_class'] = 'S3BackUpService';
 $container['updater_class'] = 'HM\\BackUpWordPress\\PluginUpdater';
+$container['prefix'] = 'aws';
