ed7156d7435d1969a3ac3bcdfa884d94dcf94703
paul de wouters <paul@hmn.md>
2015-05-16 08:54:59 +0100

    bump version

--- inc/Config.php
+++ inc/Config.php
@@ -2,7 +2,7 @@
 
 $container['addon_class'] = 'HM\\BackUpWordPress\\Addon';
 $container['checklicense_class'] = 'HM\\BackUpWordPress\\CheckLicense';
-$container['addon_version'] = '2.0.4';
+$container['addon_version'] = '2.1.0';
 $container['min_bwp_version'] = '3.1.4';
 $container['edd_download_file_name'] = 'BackUpWordPress To Amazon S3';
 $container['addon_settings'] = 'hmbkpp_aws_settings';
