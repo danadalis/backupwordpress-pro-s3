8e4e3eba2a3e0c9a2a150f7782463961ae2250d9
paul de wouters <paul@hmn.md>
2015-05-27 19:09:43 +0100

    Fixes issue with hardcoded values

--- backupwordpress-pro-s3.php
+++ backupwordpress-pro-s3.php
@@ -4,7 +4,7 @@ Plugin Name: BackUpWordPress To Amazon S3
 Plugin URI: https://bwp.hmn.md/downloads/backupwordpress-to-amazon-s3/
 Description: Send your backups to your Amazon S3 account
 Author: Human Made Limited
-Version: 2.1.1
+Version: 2.1.2
 Author URI: https://bwp.hmn.md/
 License: GPLv2
 Network: true
@@ -42,6 +42,7 @@ require __DIR__ . '/inc/Services.php';
 $addon = $container['addon'];
 
 register_activation_hook( __FILE__, array( $addon, 'maybe_self_deactivate' ) );
+register_deactivation_hook( __FILE__, array( $addon, 'deactivate' ) );
 
 $admin = $container['admin'];
 
--- inc/Config.php
+++ inc/Config.php
@@ -2,10 +2,11 @@
 
 $container['addon_class'] = 'HM\\BackUpWordPress\\Addon';
 $container['checklicense_class'] = 'HM\\BackUpWordPress\\CheckLicense';
-$container['addon_version'] = '2.1.0';
+$container['addon_version'] = '2.1.2';
 $container['min_bwp_version'] = '3.1.4';
 $container['edd_download_file_name'] = 'BackUpWordPress To Amazon S3';
 $container['addon_settings'] = 'hmbkpp_aws_settings';
 $container['service_class'] = 'S3BackUpService';
 $container['updater_class'] = 'HM\\BackUpWordPress\\PluginUpdater';
 $container['prefix'] = 'aws';
+$container['plugin_name'] = 's3';
