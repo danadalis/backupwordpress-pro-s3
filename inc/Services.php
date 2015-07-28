0aaabbd709d4cf3553e791b124763e6e5583f6f7
Paul de Wouters <paul@hmn.md>
2015-07-28 16:07:24 +0100

    Fixes updates check

--- inc/Services.php
+++ inc/Services.php
@@ -5,13 +5,9 @@ $container['bwp'] = function( $container ) {
 };
 
 $container['addon'] = function( $container ) {
-	return new $container['addon_class']( $container['addon_version'], $container['min_bwp_version'], $container['service_class'],$container['edd_download_file_name'], $container['plugin_name'], $container['addon_settings_key'], $container['addon_settings_defaults'] );
-};
-
-$container['updater'] = function( $container ) {
-	return new $container['updater_class']();
+	return new $container['addon_class']( $container['addon_version'], $container['min_bwp_version'], $container['service_class'], $container['edd_download_file_name'], $container['plugin_name'], $container['addon_settings_key'], $container['addon_settings_defaults'] );
 };
 
 $container['admin'] = function( $container ) {
-	return new $container['checklicense_class']( $container['addon_settings_key'],$container['addon_settings_defaults'], $container['edd_download_file_name'], $container['addon'], $container['updater'], $container['prefix'] );
+	return new $container['checklicense_class']( $container['addon_settings_key'],$container['addon_settings_defaults'], $container['edd_download_file_name'], $container['addon'], $container['prefix'] );
 };
