83ce1fd52770742e46c4b15cea5c3c95d505c0b5
Paul de Wouters <paul@hmn.md>
2016-03-07 17:51:16 +0000

    Version 2.1.9

--- backupwordpress-pro-s3.php
+++ backupwordpress-pro-s3.php
@@ -4,7 +4,7 @@ Plugin Name: BackUpWordPress To Amazon S3
 Plugin URI: https://bwp.hmn.md/downloads/backupwordpress-to-amazon-s3/
 Description: Send your backups to your Amazon S3 account
 Author: Human Made Limited
-Version: 2.1.8
+Version: 2.1.9
 Author URI: https://bwp.hmn.md/
 License: GPLv2
 Network: true
@@ -13,7 +13,7 @@ Domain Path: /languages
 */
 
 /*
-Copyright 2013-2014 Human Made Limited  (email : support@hmn.md)
+Copyright 2013-2016 Human Made Limited  (email : support@hmn.md)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
